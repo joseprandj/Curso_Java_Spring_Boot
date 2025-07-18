b28153aeb121da05363c63e03789f722ab6c86b8
JJ - Jose Junior <jose.junior@doalti.com.br>
2025-07-18 16:51:37 -0300

    Exception handling - findById

---
+++ src/main/java/educandoweb/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package educandoweb.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(Object id){
+        super("Resource not found. id " + id);
+    }
+}
