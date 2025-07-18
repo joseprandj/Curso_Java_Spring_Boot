3eec93da48f0dcdaa29ab8ca02f84a10d3d9742c
JJ - Jose Junior <jose.junior@doalti.com.br>
2025-07-18 15:03:29 -0300

    OrderItem, many-to-many association with extra attributes - PT 1
    B

---
+++ src/main/java/educandoweb/course/entities/pk/OrderItemPK.java
@@ -0,0 +1,48 @@
+package educandoweb.course.entities.pk;
+
+import educandoweb.course.entities.Order;
+import educandoweb.course.entities.Product;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class OrderItemPK implements Serializable {
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof OrderItemPK that)) return false;
+        return Objects.equals(order, that.order) && Objects.equals(product, that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(order, product);
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
