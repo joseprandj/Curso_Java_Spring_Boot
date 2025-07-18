baab6247a48c41fc22f596f5f32f38b3e72a21ae
JJ - Jose Junior <jose.junior@doalti.com.br>
2025-07-18 11:27:56 -0300

    Order, Instant, ISO 8601 - PT 1

--- src/main/java/educandoweb/course/entities/User.java
+++ src/main/java/educandoweb/course/entities/User.java
@@ -3,6 +3,8 @@ package educandoweb.course.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -15,6 +17,9 @@ public class User implements Serializable{
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User(){}
 
@@ -66,6 +71,10 @@ public class User implements Serializable{
         this.password = password;
     }
 
+    public List<Order> getOrders() {
+        return orders;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (!(o instanceof User user)) return false;
@@ -76,6 +85,5 @@ public class User implements Serializable{
     public int hashCode() {
         return Objects.hashCode(id);
     }
-
 
 }
